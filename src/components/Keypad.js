831d9814f4e4cca04bfd45170fe7926960c252b6
rj7shakya <shakyarajad1@gmail.com>
2022-07-15 00:24:05 +0545

    validation addded

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -7,20 +7,29 @@ const Keypad = () => {
   const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"];
   const [allkeys, setallkeys] = useState("");
 
+  const onAdd = (val) => {
+    if (allkeys.length < 10) {
+      setallkeys(allkeys + val);
+    } else {
+      alert("Maximum 10 digits allowed");
+    }
+  };
+
+  const onRemove = () => {
+    if (allkeys.length > 0) {
+      setallkeys(allkeys.substring(0, allkeys.length - 1));
+    } else {
+      alert("No digits left");
+    }
+  };
+
   return (
     <div className="main__container">
       <div className="phone__container">
-        <Field
-          allkeys={allkeys}
-          onRemove={() => setallkeys(allkeys.substring(0, allkeys.length - 1))}
-        />
+        <Field allkeys={allkeys} onRemove={onRemove} />
         <div className="phone__keypad">
           {numbers.map((i, index) => (
-            <Key
-              number={i}
-              key={index}
-              onAdd={(val) => setallkeys(allkeys + val)}
-            />
+            <Key number={i} key={index} onAdd={onAdd} />
           ))}
         </div>
       </div>
